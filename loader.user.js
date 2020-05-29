eb62043e090f613b7cf7bae09227b06f2d4c2b08
charliemin2009 <47490607+charliemin2009@users.noreply.github.com>
2020-05-29 13:40:49 +0800

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -1,1 +1,12 @@
-
+// ==UserScript==
+// @name         Blumer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Gui for Reading Garden
+// @author       You
+// @match        https://readinggarden.chinese.ephhk.com/*
+// @require      https://code.jquery.com/jquery-2.1.1.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+eval($.get("https://github.com/charliemin2009/gui/raw/master/blumer.user.js"))
